84f1f83aceb28dc1762e49aaa00e1edbbcfa928e
Dean Phillips <dean.phillips@e-hps.com>
2024-02-14 22:04:13 -0600

    added some component maps that we'll use to create random configurations / strategies for chat chains

---
+++ app/chat/llms/__init__.py
@@ -0,0 +1,12 @@
+from functools import partial
+from .chatopenai import build_llm
+
+llm_map = {
+    # The idea here is to do something like this...
+    # llm_builder = llm_map["gpt-4"]
+    # llm_builder() # produce an llm based on gpt-4
+    # partial creates a new function that calls build_llm and
+    # passes in a kwarg of gpt-4
+    "gpt-4": partial(build_llm, "gpt-4"),
+    "gpt-3.5-turbo": partial(build_llm, "gpt-3.5-turbo")
+}
