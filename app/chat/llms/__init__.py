f3b40343cd6d5cd4761a69fcfe148b66b21af43a
Dean Phillips <dean.phillips@e-hps.com>
2024-02-14 22:56:19 -0600

    randomize the chain components

--- app/chat/llms/__init__.py
+++ app/chat/llms/__init__.py
@@ -7,6 +7,6 @@ llm_map = {
     # llm_builder() # produce an llm based on gpt-4
     # partial creates a new function that calls build_llm and
     # passes in a kwarg of gpt-4
-    "gpt-4": partial(build_llm, "gpt-4"),
-    "gpt-3.5-turbo": partial(build_llm, "gpt-3.5-turbo")
+    "gpt-4": partial(build_llm, model_name="gpt-4"),
+    "gpt-3.5-turbo": partial(build_llm, model_name="gpt-3.5-turbo")
 }
